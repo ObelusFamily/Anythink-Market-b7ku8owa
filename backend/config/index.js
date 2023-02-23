234b237209059355db97ae3a2a3d06e3779ad47b
Nonne <42183296+Nonne46@users.noreply.github.com>
2023-02-23 15:51:19 +0000

    Add test token

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
   secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret',
-  openai_token: process.env.OPENAITOKEN
+  openai_token: process.env.NODE_ENV === 'production' ? process.env.OPENAITOKEN : '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',
 };
